b0b4bec5573464d8a54d5a8a38ae007197a6363c
Azubogu Kosiso <azuboguko@gmail.com>
2026-05-19 20:12:03 +0100

    Refactor Redis connection configuration

--- backend/src/utils/redis.ts
+++ backend/src/utils/redis.ts
@@ -1,9 +1,10 @@
 import Redis from "ioredis";
 
 const redisConnection = new Redis(
-  process.env.REDIS_URL || "redis://localhost:6379",
+  process.env.REDIS_URL as string,
   {
-    maxRetriesPerRequest: null,
+    lazyConnect: true,
+    maxRetriesPerRequest: 1,
     tls: process.env.NODE_ENV === "production" ? {} : undefined,
   },
 );
